520f644308e219e9678fbf0b0ace344032a4a46b
Anurag Mohanty <anuragmohanty@macbook-air.lan>
2024-09-21 01:34:08 -0400

    Serve production build

--- geography-bee-backend/server.js
+++ geography-bee-backend/server.js
@@ -10,6 +10,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'build')));
+
+// The "catchall" handler: for any request that doesn't
+// match an API route, send back the React index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
 let drive;
 try {
     const auth = new google.auth.GoogleAuth({
